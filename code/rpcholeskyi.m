17138e920e2b6a940f32fbbd8fc7b5251a3452ce
Ethan Epperly <epperlyethan@gmail.com>
2022-11-07 09:03:36 -0800

    Add implicit version of RPCholesky

---
+++ code/rpcholeskyi.m
@@ -0,0 +1,19 @@
+function [F,AS,S] = rpcholeskyi(A,d,k,B)
+% RPCHOLESKYI An implicit version of RPCholesky which takes as input a
+% *function* A which takes as input an index set and outputs the columns of
+% A corresponding to that set. Second input specifies the diagonal.
+
+N = length(d);
+F = zeros(N,k); AS = zeros(N,k); S = zeros(k,1); i = 0;
+while i < k
+    s = unique(randsample(N,min(B,k-i),true,d));
+    S(i+1:i+length(s)) = s; 
+    AS = A(s);
+    G = AS - F(:,1:i) * F(s,1:i)';
+    R = chol(G(s,:));
+    F(:,i+1:i+length(s)) = G / R;
+    d = max(d - vecnorm(F(:,i+1:i+length(s)),2,2) .^ 2,0);
+    i = i+length(s);
+end
+end
+
