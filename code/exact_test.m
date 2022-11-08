b745d72daede2e4c03987f26ee075bddd5c5bed4
Ethan Epperly <epperlyethan@gmail.com>
2022-11-08 11:27:45 -0800

    Fix test script

--- code/exact_test.m
+++ code/exact_test.m
@@ -1,6 +1,6 @@
 %% Initialize data
 load('../data/homo.mat')
-N = 1e3;
+N = 1e4;
 X_test = X(N+1:(2*N),:); Y_test = Y(N+1:(2*N));
 X = X(1:N,:); Y = Y(1:N);
 
@@ -20,14 +20,11 @@ a = 5120;
 A = exp(-pdist2(X,X,"minkowski",1)/a);
 
 % Stats
-test_eval = @(beta) kernel(X_test,X) * beta;
-test_accuracy = @(beta) norm(test_eval(beta) - Y_test,1) / length(Y_test);
 relres = @(beta) norm(A*beta + mu*beta - Y) / norm(Y);
-summary = @(beta) [relres(beta) test_accuracy(beta)];
 
 %% Run with RPCholesky and without
-[~,rpcholesky] = krr(A,mu,Y,100,[],summary,'rpcnys');
-[~,noprec] = krr(A,mu,Y,100,[],summary,'');
+[~,rpcholesky] = krr(A,mu,Y,100,[],relres,'rpcnys');
+[~,noprec] = krr(A,mu,Y,100,[],relres,'');
 semilogy(rpcholesky(:,1)); hold on; semilogy(noprec(:,1))
 xlabel('Iteration'); ylabel('Relative Residual')
 legend({'RPCholesky','No Preconditioner'})
