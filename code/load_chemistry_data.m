157bc5bccd9251d15c4ed5c1b4266eac5f1bd919
Ethan Epperly <epperlyethan@gmail.com>
2022-11-11 08:46:10 -0800

    Add test for approximate KRR

---
+++ code/load_chemistry_data.m
@@ -0,0 +1,19 @@
+%% Initialize data
+load('../data/homo.mat')
+X_test = X(N+1:(2*N),:); Y_test = Y(N+1:(2*N));
+X = X(1:N,:); Y = Y(1:N);
+
+% Standardization
+X_mean = mean(X); X_std = std(X);
+bad_idx = find(std(X) == 0);
+X(:,bad_idx) = []; X_test(:,bad_idx) = [];
+X_mean(bad_idx) = []; X_std(bad_idx) = [];
+X = (X - X_mean) ./ X_std;
+X_test = (X_test - X_mean) ./ X_std;
+
+% Hyperparameters
+mu = N*1.0e-8;
+a = 5120;
+
+% Kernel
+kernel = @(X1,X2) exp(-pdist2(X1,X2,"minkowski",1)/a);
