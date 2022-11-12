05483efa9f5bf8db1f092626d06ed0878c948207
Ethan Epperly <epperlyethan@gmail.com>
2022-11-12 15:26:07 -0800

    Fix bug in Gaussian Nystrom

--- code/gauss_nystrom.m
+++ code/gauss_nystrom.m
@@ -1,12 +1,12 @@
 function [F,nu] = gauss_nystrom(A,k)
-[n,~] = size(A,1);
-Omega = orth(randn(n,k)); %Generate test matrix
+N = size(A,1);
+Omega = orth(randn(N,k)); %Generate test matrix
 if isfloat(A)
     Y = A*Omega; % Compute sketch
 else
     Y = kermul(A,Omega);
 end
-nu = sqrt(n)*eps(norm(Y,2)); %Compute shift
+nu = sqrt(N)*eps(norm(Y,2)); %Compute shift
 Y = Y+nu*Omega;
 B = Omega'*Y;
 C = chol(B);
