6c611766401c57358b2092d15822af2ef70884f4
Ethan Epperly <epperlyethan@gmail.com>
2022-11-12 09:50:03 -0800

    Add different preconditioners to approximate_krr.m

--- code/approximate_krr.m
+++ code/approximate_krr.m
@@ -19,14 +19,40 @@ else
     tol = 0;
 end
 
-N = size(A_S,1); k = size(A_S,2); d = 2*k;
-C = chol(A_SS + trace(A_SS)*eps*eye(size(A_SS,1)));
+if length(varargin) > 3
+    precname = varargin{4};
+else
+    precname = 'spqr';
+end
 
-Phi = sparse_sign(d,N,8); PhiA_S = Phi * A_S;
-B = [PhiA_S;sqrt(mu)*C];
-[Q,R] = qr(B,'econ');
+N = size(A_S,1); k = size(A_S,2); 
 
+if contains(precname, 'sp')
+    d = 2*k;
+    Phi = sparse_sign(d,N,8);
+    PhiA_S = Phi * A_S;
+
+    if contains(precname, 'qr')
+        C = chol(A_SS + trace(A_SS)*eps*eye(size(A_SS,1)));
+        B = [PhiA_S;sqrt(mu)*C];
+        [Q,R] = qr(B,'econ');
+        w0 = R \ (Q(1:d,:)' * (Phi*y));
+    else
+        H = PhiA_S'*PhiA_S + A_SS;
+        R = chol(H + trace(H)*eps*eye(k));
+        w0 = zeros(k,1);
+    end
+    prec = @(x) R \ (R'\x);
+elseif contains(precname,'falkon')
+    T = chol(A_SS + trace(A_SS)*eps*eye(k));
+    R = chol(N/k * (T*T') + mu*eye(k));
+    prec = @(x) T \ (R \ (R' \ (T' \ x)));
+    w0 = zeros(k,1);
+else 
+    error('"%s" preconditioner not implemented',precname)
+end
+    
 matvec = @(x) A_S' * (A_S * x) + mu * A_SS * x;
-[w,~,stats] = mycg(matvec,@(x) R \ (R'\x),A_S'*y,tol,numiters,summary,...
-    R \ (Q(1:d,:)'*(Phi*y)));
+ASy = A_S'*y;
+[w,~,stats] = mycg(matvec,prec,ASy,tol,numiters,summary,w0);
 end
