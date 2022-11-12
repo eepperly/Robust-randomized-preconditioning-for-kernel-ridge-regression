0f660eddde421f719eff8c5d5510cc28285e77d6
Ethan Epperly <epperlyethan@gmail.com>
2022-11-12 09:50:28 -0800

    Update lines_crossing.m to compare different preconditioners

--- code/lines_crossing.m
+++ code/lines_crossing.m
@@ -3,17 +3,23 @@
 load_higgs
 
 %% Test
-ks = round(logspace(1,4,20));
+ks = round(logspace(3,4,8));
 direct_times = [];
-approx_times = [];
+qr_times = [];
+chol_times = [];
+falkon_times = [];
 for k = ks
     fprintf('%d\n',k)
     S = randsample(N,k,false);
     A_S = kernel(X,X(S,:));
     A_SS = A_S(S,:);
 
-    tic; approximate_krr(A_S,A_SS,mu,Y,[],100,1e-5);
-    approx_times(end+1) = toc;
+    tic; approximate_krr(A_S,A_SS,mu,Y,[],100,1e-5,'spqr');
+    qr_times(end+1) = toc;
+    tic; approximate_krr(A_S,A_SS,mu,Y,[],100,1e-5,'spchol');
+    chol_times(end+1) = toc;
+    tic; approximate_krr(A_S,A_SS,mu,Y,[],100,1e-5,'falkon');
+    falkon_times(end+1) = toc;
     tic; w = (A_S'*A_S + mu*A_SS) \ (A_S'*Y);
     direct_times(end+1) = toc;
 end
@@ -21,13 +27,15 @@ end
 %% Figure
 close all
 figure(1)
-loglog(ks,approx_times,'LineWidth',3)
+loglog(ks,qr_times,'LineWidth',3)
 hold on
+loglog(ks,chol_times,':','LineWidth',3)
+loglog(ks,falkon_times,':','LineWidth',3)
 loglog(ks,direct_times,'--','LineWidth',3)
-axis([1e2 1e4 -Inf Inf])
+axis([1e3 1e4 -Inf Inf])
 xlabel('Number of Centers $k$')
 ylabel('Computation Time (sec)')
-legend({'Sketch and Precondition','Direct'},'FontSize',20,...
+legend({'SP-QR','SP-Chol','FALKON','Direct'},'FontSize',20,...
     'Location','southeast')
 set(gca,'FontSize',20)
 
