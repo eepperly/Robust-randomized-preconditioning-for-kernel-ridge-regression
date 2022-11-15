e9658eee68bdff9765857eeacfb26447b5ba43e8
Mateo Diaz <mateodd@me.com>
2022-11-14 21:37:15 -0800

    Deleted repetition for standarize and kernelmatrix

---
+++ utils/kernelmatrix.m
@@ -0,0 +1,12 @@
+function K = kernelmatrix(X1, X2, kerneltype, bandwidth)
+%KERNELMATRIX Computes the kernel matrix K(X1, X2).
+%   Computes the kernel matrix of the two samples. Valid options for the 
+% kernel type are 'gaussian' and 'laplace'. 
+if contains(kerneltype, 'gaussian')
+    K = exp(-pdist2(X1,X2,"euclidean").^2 / (2*bandwidth));
+elseif contains(kerneltype, 'laplace')
+    K = exp(-pdist2(X1,X2,"minkowski",1)/bandwidth);
+else
+    error('Kernel ' + kerneltype + ' not implemented')
+end
+end
