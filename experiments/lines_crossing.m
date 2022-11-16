6682ec0c791b60c7c5d1901b9151fb8eb33231f8
Ethan Epperly <epperlyethan@gmail.com>
2022-11-16 08:21:45 -0800

    Move lines_crossing.m to experiments folder

--- experiments/lines_crossing.m
+++ experiments/lines_crossing.m
@@ -1,9 +1,11 @@
 %% Set up workspace
 close all; clear; clc
 addpath('../utils')
+addpath('../code')
 
 %% Load data
 load_higgs
+verbose = true;
 
 %% Test
 ks = round(logspace(3,4,11));
@@ -17,9 +19,9 @@ for k = ks
     A_S = kernel(X,X(S,:));
     A_SS = A_S(S,:);
 
-    tic; approximate_krr(A_S,A_SS,mu,Y,[],100,1e-5,'spchol');
+    tic; approximate_krr(A_S,A_SS,mu,Y,[],100,1e-5,'spchol',verbose);
     chol_times(end+1) = toc;
-    tic; approximate_krr(A_S,A_SS,mu,Y,[],100,1e-5,'falkon');
+    tic; approximate_krr(A_S,A_SS,mu,Y,[],100,1e-5,'falkon',verbose);
     falkon_times(end+1) = toc;
     tic; w = (A_S'*A_S + mu*A_SS) \ (A_S'*Y);
     direct_times(end+1) = toc;
@@ -27,12 +29,13 @@ end
 
 %% Figure
 close all
+loadColors
 figure(1)
-loglog(ks,chol_times,'-','LineWidth',3)
+loglog(ks,chol_times,'-','LineWidth',3,'Color',color1)
 hold on
-loglog(ks,falkon_times,':','LineWidth',3)
-loglog(ks,direct_times,'--','LineWidth',3)
-axis([1e3 1e4 -Inf Inf])
+loglog(ks,falkon_times,':','LineWidth',3,'Color',color2)
+loglog(ks,direct_times,'--','LineWidth',3,'Color',color3)
+axis([min(ks) max(ks) -Inf Inf])
 xlabel('Number of Centers $k$')
 ylabel('Computation Time (sec)')
 legend({'Sketch and Precondition','FALKON','Direct'},'FontSize',20,...
