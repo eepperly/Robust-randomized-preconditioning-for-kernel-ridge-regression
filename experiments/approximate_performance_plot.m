4f03d6222a0beae8ad99bb32b95e90fbd9824842
Mateo Diaz <mateodd@me.com>
2022-11-18 09:29:52 -0800

    Improvements to approximate_performance_plot.m

--- experiments/approximate_performance_plot.m
+++ experiments/approximate_performance_plot.m
@@ -7,12 +7,12 @@ resultsPath = createFolderForExecution("approximate_performance_plot");
 
 %% Parameters
 rng(926); % For reproducibility purposes
-N = 45000;
-k = 250; % Change to generate different plots
-Nts = 100;
+N = 50000;
+k = 225; % Change to generate different plots
+Nts = 10000;
 mu = 1e-8 * N;
-bandwidth = 100;
-num_iter = 50;
+bandwidth = 20;
+num_iter = 100;
 kernel = "gaussian";
 tol = 1e-9;
 
@@ -83,31 +83,33 @@ for j = 1:numel(names)
     fprintf('\tNo preconditioner iters: %d last iter error: %7.2e\n', size(results.(names{j}).noprec, 1), results.(names{j}).noprec(end, 1));
     
     f1 = figure(2*j - 1);
-    semilogy(results.(names{j}).krill(:,1), 'Color', color3)
+    semilogy(results.(names{j}).krill(:,1),'Linewidth', 4, 'Color', color3)
     hold on
-    semilogy(results.(names{j}).falkon(:,1), 'Color', color4)
-    semilogy(results.(names{j}).noprec(:,1), 'Color', color2)
+    semilogy(results.(names{j}).falkon(:,1),'Linewidth', 4, 'Color', color5)
+    semilogy(results.(names{j}).noprec(:,1),'Linewidth', 4, 'Color', color2)
     xlabel('Iteration'); ylabel('Relative Residual')
-    legend({'KRILL (Ours)','FALKON'})
+    legend({'KRILL (Ours)','FALKON', "No preconditioner"})
+    set(gca,'FontSize',20)
 
     f2 = figure(2*j);
-    semilogy(results.(names{j}).krill(:,2), 'Color', color3)
+    semilogy(results.(names{j}).krill(:,2),'Linewidth', 4, 'Color', color3)
     hold on
-    semilogy(results.(names{j}).falkon(:,2), 'Color', color5)
-    semilogy(results.(names{j}).noprec(:,2), 'Color', color2)
+    semilogy(results.(names{j}).falkon(:,2),'Linewidth', 4, 'Color', color5)
+    semilogy(results.(names{j}).noprec(:,2),'Linewidth', 4, 'Color', color2)
     xlabel('Iteration'); ylabel('Test error')
     legend({'KRILL (Ours)','FALKON', "No preconditioner"})
-
-    saveas(f1,fullfile(resultsPath, string(names{j}) +'_exact_test_res.fig'))
-    saveas(f1,fullfile(resultsPath, string(names{j}) +'_exact_test_res.png'))
-    saveas(f2,fullfile(resultsPath, string(names{j}) +'_exact_test_error.fig'))
-    saveas(f2,fullfile(resultsPath, string(names{j}) +'_exact_test_error.png'))
+    set(gca,'FontSize',20)
+    saveas(f1,fullfile(resultsPath, string(names{j}) +'_res.fig'))
+    saveas(f1,fullfile(resultsPath, string(names{j}) +'_res.png'))
+    saveas(f2,fullfile(resultsPath, string(names{j}) +'_test_error.fig'))
+    saveas(f2,fullfile(resultsPath, string(names{j}) +'_test_error.png'))
 end
 
 %% Generate performance plot
 loadColors
 density = zeros(num_iter,3);
-accuracy = 1e-6 ;
+names = fieldnames(problems);
+accuracy = 1e-8;
 for j = 1:numel(names)
    density(min(find(results.(names{j}).krill(:,1) <= accuracy)), 1) = density(min(find(results.(names{j}).krill(:,1) <= accuracy)), 1) + 1;
    density(min(find(results.(names{j}).falkon(:,1) <= accuracy)), 2) = density(min(find(results.(names{j}).falkon(:,1) <= accuracy)), 2) + 1;
@@ -126,26 +128,33 @@ numberproblems = numel(names);
 
 plot(cumulative(:, 2)/numberproblems, 'Linewidth', 4, 'Color', color5) % FALKON
 hold on
-plot(cumulative(:, 3)/numberproblems, 'Linewidth', 4, 'Color', color2) % No Prec
+plot(cumulative(:, 3)/numberproblems, 'Linewidth', 4, 'Color', color2, 'LineStyle', '--') % No Prec
 plot(cumulative(:, 1)/numberproblems, 'Linewidth', 4, 'Color', color3) % KRILL
 ylim([0.0 1.0])
 xlabel('Iteration', 'FontSize', 24); 
 ylabel('Fraction of solved problems', 'FontSize', 24)
-le = legend({'FALKON', 'No preconditioner', 'KRILL (Ours)'});
+le = legend({'FALKON', 'No preconditioner', 'KRILL (Ours)'}, 'Location', 'northwest');
 set(gca,'FontSize',20)
-axis([0 50 0 1])
-saveas(fperformance,fullfile(resultsPath, 'performance.fig'))
-exportgraphics(fperformance,fullfile(resultsPath, 'performance.png'), 'Resolution', 300)
+axis([0 num_iter 0 1])
+saveas(fperformance,fullfile(resultsPath, accuracy + "_" + k + "_performance.fig"))
+exportgraphics(fperformance,fullfile(resultsPath, accuracy + "_" + k + "_performance.png"), 'Resolution', 300)
 
 %% Save everything
-save(fullfile(resultsPath, 'state.mat'), 'problems', 'results', 'N', 'mu', 'bandwidth', 'k' )
+save(fullfile(resultsPath, 'state.mat'), 'problems', 'results', 'num_iter', 'N', 'mu', 'bandwidth', 'k', 'resultsPath' )
 
 %% Auxiliary functions
 function [Xtr, Ytr, Xts, Yts] = subsample(Xtr, Ytr, Xts, Yts, Ntr, Nts)
     n = length(Ytr);
     idx_resh =randperm(n,n);
+    nts = length(Yts);
+    idx_resh_ts =randperm(nts,nts);
+
     Xtr = Xtr(idx_resh,:);
     Ytr = Ytr(idx_resh);
+    
+    Xts = Xts(idx_resh_ts,:);
+    Yts = Yts(idx_resh_ts);
+
 
     if Ntr < size(Xtr, 1)
         Xtr = Xtr(1:Ntr,:); Ytr = Ytr(1:Ntr);
