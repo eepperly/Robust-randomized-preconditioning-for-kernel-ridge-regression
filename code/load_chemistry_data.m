1e297030b01ba7afcee87859d7e134922a284c54
Ethan Epperly <epperlyethan@gmail.com>
2022-12-12 15:09:34 -0800

    Add bounds for load_chemistry_data.m

--- code/load_chemistry_data.m
+++ code/load_chemistry_data.m
@@ -1,7 +1,7 @@
 %% Initialize data
 addpath('../utils')
 load('../data/homo.mat')
-X_test = X(N+1:(2*N),:); Y_test = Y(N+1:(2*N));
+X_test = X(N+1:min(2*N,end),:); Y_test = Y(N+1:min(2*N,end));
 X = X(1:N,:); Y = Y(1:N);
 
 % Standardization
