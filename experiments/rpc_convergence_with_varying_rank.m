f47128140d4cdaef535526afea7616ba5a091522
Ethan Epperly <epperlyethan@gmail.com>
2023-03-20 09:17:30 -0700

    Update iterations vs r script to have other preconditioners

--- experiments/rpc_convergence_with_varying_rank.m
+++ experiments/rpc_convergence_with_varying_rank.m
@@ -8,45 +8,76 @@ rng('default')
 %% Set options
 verbose = true;
 N = 64e3;
-ks = [1e2 2e2 5e2 1e3 2e3 4e3, 8e3, 16e3, 32e3];
+ks = [1e2 2e2 5e2 1e3 2e3 5e3, 1e4, 2e4];
 numiters = 1000;
-pcgtol = 1e-4;
+pcgtol = 1e-3;
 choltol = 1e-9;
 load_chemistry_data
+
 %% Kernel matrix
 
 fprintf('Building kernel matrix... ')
 A = kernel(X,X);
-Atest = kernel(X_test,X);
 fprintf('done!\n')
 
-test_accuracy = @(beta) norm(Atest*beta - Y_test,1) / length(Y_test);
 relres = @(beta) norm(A*beta + mu*beta - Y) / norm(Y);
-summary = @(beta) [relres(beta) test_accuracy(beta)];
+summary = @(beta) [relres(beta)];
 
 %% Experiment
-ks = [1e2 2e2 5e2 1e3 2e3 5e3, 1e4, 2e4]
-results = zeros(numiters, 2, length(ks));
-final_iter = zeros(length(ks));
+results = zeros(numiters, length(ks));
+final_iter = zeros(1, length(ks));
 counter = 1;
 for k = ks
     fprintf("Solving k %d... ", k);
-    [~, result] = krr(A, mu, Y, k, [], summary, 'rpcnys', numiters, choltol, pcgtol);
-    results(1:length(result(:, 1)), :, k) = result;
-    fprintf('done! Number of iterations: %d\n', length(result(:, 1)));
-    final_iter(counter) = length(result(:, 1));
+    [~, result] = krr(A, mu, Y, k, [], summary, 'rpcnys', numiters, choltol, pcgtol, true);
+    results(1:length(result), counter) = result;
+    fprintf('done! Number of iterations: %d\n', length(result));
+    final_iter(counter) = length(result);
     counter = counter + 1;
-end 
+end
+
+%% Experiment - uniform
+results_uniform = zeros(numiters, length(ks));
+final_iter_uniform = zeros(1, length(ks));
+counter = 1;
+for k = ks
+    fprintf("Solving k %d... ", k);
+    [~, result] = krr(A, mu, Y, k, [], summary, 'uninys', numiters, choltol, pcgtol, true);
+    results_uniform(1:length(result), counter) = result;
+    fprintf('done! Number of iterations: %d\n', length(result));
+    final_iter_uniform(counter) = length(result);
+    counter = counter + 1;
+end
+
+%% Experiment - greedy
+results_greedy = zeros(numiters, length(ks));
+final_iter_greedy = zeros(1, length(ks));
+counter = 1;
+for k = ks
+    fprintf("Solving k %d... ", k);
+    [~, result] = krr(A, mu, Y, k, [], summary, 'greedynys', numiters, choltol, pcgtol, true);
+    results_greedy(1:length(result), counter) = result;
+    fprintf('done! Number of iterations: %d\n', length(result));
+    final_iter_greedy(counter) = length(result);
+    counter = counter + 1;
+end
 
 %% Plot 
 fperformance = figure();
 loadColors
 loadFont
-
+semilogx(ks, final_iter_greedy, '-.', 'LineWidth', 3,'Color',color1)
+hold on
+semilogx(ks, final_iter_uniform, '--', 'LineWidth', 4, 'Color', color4)
 semilogx(ks, final_iter, 'Linewidth', 4, 'Color', color3)
-xlabel('Number of centers'); ylabel('Iterations to convergence');
+hold off
+xlabel('Approximation rank $r$'); ylabel('Iterations to convergence');
 set(gca, 'FontSize', 22);
+legend({'Greedy', 'Uniform','RPCholesky'},'location','best','FontSize',20)
+
 %% Save everything
 saveas(fperformance,fullfile(resultsPath, 'convergence_vs_rank.fig'))
 exportgraphics(fperformance,fullfile(resultsPath, 'convergence_vs_rank.png'), 'Resolution',300)
-save(fullfile(resultsPath, 'state.mat'), 'results', 'N', 'mu', 'bandwidth', 'ks', 'final_iter')
+save(fullfile(resultsPath, 'state.mat'), 'results', 'results_uniform', ...
+    'results_greedy', 'final_iter_greedy', 'final_iter_uniform', ...
+    'final_iter', 'N', 'mu', 'bandwidth', 'ks')
